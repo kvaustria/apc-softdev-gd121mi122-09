2db74ddec73f8e4fc0eb431987f5d72cd114439b
Ruby Elizabeth Bisbal <rizzabisbal@gmail.com>
2015-07-21 13:00:07 +0800

    updated db

--- practice/midterm_test/Bisbal_Ruby_Elizabeth/ourspace/config/db.php
+++ practice/midterm_test/Bisbal_Ruby_Elizabeth/ourspace/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=softdev_bisbal_ruby_elizabeth_ourspace',
+    'dsn' => 'mysql:host=localhost;dbname=softdev_bisbal_ruby_ourspace',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
